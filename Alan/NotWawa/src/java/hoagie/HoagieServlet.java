527aaf83b949466bce48565eaec7f767f1f3d050
Alan Morin <root.user0@gmail.com>
2012-11-10 17:16:37 -0500

    updated HoagieServlet

--- Alan/NotWawa/src/java/hoagie/HoagieServlet.java
+++ Alan/NotWawa/src/java/hoagie/HoagieServlet.java
@@ -6,6 +6,8 @@ package hoagie;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.ejb.EJB;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,8 +17,13 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Alan
  */
+@WebServlet(name="HoagieServlet", urlPatterns={"/hoagie"})
 public class HoagieServlet extends HttpServlet {
-
+    private static final long serialVersionUID = 1L;
+ 
+    // Injected DAO EJB:
+    @EJB HoagieDao hoagieDao;
+    
     /**
      * Processes requests for both HTTP
      * <code>GET</code> and
@@ -27,63 +34,29 @@ public class HoagieServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+   @Override
+    protected void doGet(
+        HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        try {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet HoagieServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet HoagieServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        } finally {            
-            out.close();
-        }
+ 
+        // Display the list of guests:
+        //request.setAttribute("guests", guestDao.getAllGuests());
+        //request.getRequestDispatcher("/guest.jsp").forward(request, response);
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP
-     * <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+ 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(
+        HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+ 
+        // Handle a new guest:
+        String name = request.getParameter("name");
+        //if (name != null)
+        //    guestDao.persist(new Guest(name));
+ 
+        // Display the list of guests:
+        //doGet(request, response);
     }
-
-    /**
-     * Handles the HTTP
-     * <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 }
+
+    
