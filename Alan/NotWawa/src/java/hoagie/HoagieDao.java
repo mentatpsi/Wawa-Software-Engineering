c55670990f35f017f065ccec179822257fef593b
Alan Morin <root.user0@gmail.com>
2012-11-10 16:54:48 -0500

    Added Hoagie classes

---
+++ Alan/NotWawa/src/java/hoagie/HoagieDao.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hoagie;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Alan
+ */
+@Stateless
+public class HoagieDao {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}
