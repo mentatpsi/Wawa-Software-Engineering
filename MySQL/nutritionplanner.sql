cd15d77d5db073ae58f4a29f0da560bb3b9e78fb
Shay Maor <shay.h.maor@gmail.com>
2012-11-12 11:14:26 -0500

    [MySQL] Updates, nutrition view (phase 1) created
    Nutrition view created which keeps track of all nutritional facts of ingredients in each sandwich. Phase 2 requires a group by code. This was the code used to create the Nutrition1 view:
    
    Create View Nutrition1 AS
    SELECT SI.`Sandwich_id`, Ingr.*
    FROM (Sandwich_Ingredient SI, Ingredients Ingr)
    INNER JOIN Ingredients
    ON SI.`Ingredient_id` = Ingr.NameID
    ORDER BY SI.`Sandwich_id`

--- MySQL/nutritionplanner.sql
+++ MySQL/nutritionplanner.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 50.63.244.74
--- Generation Time: Oct 05, 2012 at 09:46 AM
+-- Generation Time: Nov 12, 2012 at 09:08 AM
 -- Server version: 5.0.92
 -- PHP Version: 5.1.6
 
@@ -22,6 +22,79 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `Ingredients`
+--
+
+CREATE TABLE `Ingredients` (
+  `NameID` varchar(255) NOT NULL,
+  `ServingSize` int(11) NOT NULL,
+  `Category` varchar(255) NOT NULL,
+  `Calories` int(11) NOT NULL,
+  `TotalFat` int(11) NOT NULL,
+  `SaturatedFat` int(11) NOT NULL,
+  `Carbohydrates` varchar(11) NOT NULL,
+  `Sodium` int(11) NOT NULL,
+  `Protein` int(11) NOT NULL,
+  `Fiber` int(11) NOT NULL,
+  `TransFat` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Ingredients`
+--
+
+INSERT INTO `Ingredients` VALUES('Salami', 0, 'Deli Meats', 25, 3, 1, '0', 110, 1, 40, 0);
+INSERT INTO `Ingredients` VALUES('Provolone', 1, 'Cheese', 50, 4, 3, '0', 100, 3, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `Italian`
+--
+CREATE TABLE `Italian` (
+`Ingredient_id` varchar(255)
+);
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `ItalianNutrition`
+--
+CREATE TABLE `ItalianNutrition` (
+`Ingredient_id` varchar(255)
+,`NameID` varchar(255)
+,`ServingSize` int(11)
+,`Category` varchar(255)
+,`Calories` int(11)
+,`TotalFat` int(11)
+,`SaturatedFat` int(11)
+,`Carbohydrates` varchar(11)
+,`Sodium` int(11)
+,`Protein` int(11)
+,`Fiber` int(11)
+,`TransFat` int(11)
+);
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `Nutrition1`
+--
+CREATE TABLE `Nutrition1` (
+`Sandwich_id` varchar(255)
+,`NameID` varchar(255)
+,`ServingSize` int(11)
+,`Category` varchar(255)
+,`Calories` int(11)
+,`TotalFat` int(11)
+,`SaturatedFat` int(11)
+,`Carbohydrates` varchar(11)
+,`Sodium` int(11)
+,`Protein` int(11)
+,`Fiber` int(11)
+,`TransFat` int(11)
+);
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `Sandwiches`
 --
 
@@ -35,6 +108,7 @@ CREATE TABLE `Sandwiches` (
 -- Dumping data for table `Sandwiches`
 --
 
+INSERT INTO `Sandwiches` VALUES('Italian', '', '');
 
 -- --------------------------------------------------------
 
@@ -52,34 +126,10 @@ CREATE TABLE `Sandwich_Ingredient` (
 -- Dumping data for table `Sandwich_Ingredient`
 --
 
+INSERT INTO `Sandwich_Ingredient` VALUES('Italian', 'Salami', 1);
+INSERT INTO `Sandwich_Ingredient` VALUES('Italian', 'Provolone', 0);
 
 -- --------------------------------------------------------
-
---
--- Table structure for table `Ingredients`
---
-
-CREATE TABLE `Ingredients` (
-  `NameID` varchar(255) NOT NULL,
-  `ServingSize` int(11) NOT NULL,
-  `Category` VarChar(255) NOT NULL,
-  `Calories` int(11) NOT NULL,
-  `TotalFat` int(11) NOT NULL,
-  `SaturatedFat` int(11) NOT NULL,
-  `Carbohydrates` varchar(11) NOT NULL,
-  `Sodium` int(11) NOT NULL,
-  `Protein` int(11) NOT NULL,
-  `Fiber` int(11) NOT NULL,
-  `TransFat` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `Ingredients`
---
-
-
--- --------------------------------------------------------
-
 
 --
 -- Table structure for table `Users`
@@ -95,3 +145,30 @@ CREATE TABLE `Users` (
 -- Dumping data for table `Users`
 --
 
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `Italian`
+--
+DROP TABLE IF EXISTS `Italian`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`nutritionplanner`@`%` SQL SECURITY DEFINER VIEW `nutritionplanner`.`Italian` AS select `nutritionplanner`.`Sandwich_Ingredient`.`Ingredient_id` AS `Ingredient_id` from `nutritionplanner`.`Sandwich_Ingredient` where (`nutritionplanner`.`Sandwich_Ingredient`.`Sandwich_id` = _utf8'Italian');
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `ItalianNutrition`
+--
+DROP TABLE IF EXISTS `ItalianNutrition`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`nutritionplanner`@`%` SQL SECURITY DEFINER VIEW `nutritionplanner`.`ItalianNutrition` AS select `Italian`.`Ingredient_id` AS `Ingredient_id`,`nutritionplanner`.`Ingredients`.`NameID` AS `NameID`,`nutritionplanner`.`Ingredients`.`ServingSize` AS `ServingSize`,`nutritionplanner`.`Ingredients`.`Category` AS `Category`,`nutritionplanner`.`Ingredients`.`Calories` AS `Calories`,`nutritionplanner`.`Ingredients`.`TotalFat` AS `TotalFat`,`nutritionplanner`.`Ingredients`.`SaturatedFat` AS `SaturatedFat`,`nutritionplanner`.`Ingredients`.`Carbohydrates` AS `Carbohydrates`,`nutritionplanner`.`Ingredients`.`Sodium` AS `Sodium`,`nutritionplanner`.`Ingredients`.`Protein` AS `Protein`,`nutritionplanner`.`Ingredients`.`Fiber` AS `Fiber`,`nutritionplanner`.`Ingredients`.`TransFat` AS `TransFat` from (`nutritionplanner`.`Italian` join `nutritionplanner`.`Ingredients` on((`Italian`.`Ingredient_id` = `nutritionplanner`.`Ingredients`.`NameID`))) order by `Italian`.`Ingredient_id`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `Nutrition1`
+--
+DROP TABLE IF EXISTS `Nutrition1`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`nutritionplanner`@`%` SQL SECURITY DEFINER VIEW `nutritionplanner`.`Nutrition1` AS select `SI`.`Sandwich_id` AS `Sandwich_id`,`Ingr`.`NameID` AS `NameID`,`Ingr`.`ServingSize` AS `ServingSize`,`Ingr`.`Category` AS `Category`,`Ingr`.`Calories` AS `Calories`,`Ingr`.`TotalFat` AS `TotalFat`,`Ingr`.`SaturatedFat` AS `SaturatedFat`,`Ingr`.`Carbohydrates` AS `Carbohydrates`,`Ingr`.`Sodium` AS `Sodium`,`Ingr`.`Protein` AS `Protein`,`Ingr`.`Fiber` AS `Fiber`,`Ingr`.`TransFat` AS `TransFat` from ((`nutritionplanner`.`Sandwich_Ingredient` `SI` join `nutritionplanner`.`Ingredients` `Ingr`) join `nutritionplanner`.`Ingredients` on((`SI`.`Ingredient_id` = `Ingr`.`NameID`))) order by `SI`.`Sandwich_id`;
