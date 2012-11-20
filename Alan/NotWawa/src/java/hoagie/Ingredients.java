a5eabb0dd6f1edbed64fb0a33eff5a4d57a3216a
Alan Morin <root.user0@gmail.com>
2012-11-19 22:10:15 -0500

    updated HoagieServlet

--- Alan/NotWawa/src/java/hoagie/Ingredients.java
+++ Alan/NotWawa/src/java/hoagie/Ingredients.java
@@ -18,6 +18,9 @@ public class Ingredients implements Serializable {
     @Id @GeneratedValue
     Long id;
     private String name;
+    private int cal, fatCal, totFat, satFat, transFat,
+            chol, sodium, carb, fiber, sugars, protein, vitA,
+            vitC, calcium, iron = 0;
 
     // Constructors:
     public Ingredients() {
@@ -27,6 +30,72 @@ public class Ingredients implements Serializable {
         this.name = name;
     }
  
+    public void setAll(int cal, int fatCal, int totFat, int satFat,
+            int transFat, int chol, int sodium, int carb, int fiber,
+            int sugars, int protein, int vitA, int vitC, int calcium,
+            int iron){
+        setCal(cal);
+        setFatCal(fatCal);
+        setTotFat(totFat);
+        setSatFat(satFat);
+        setTransFat(transFat);
+        setChol(chol);
+        setSodium(sodium);
+        setCarb(carb);
+        setFiber(fiber);
+        setSugars(sugars);
+        setProtein(protein);
+        setVitA(vitA);
+        setVitC(vitC);
+        setCalcium(calcium);
+        setIron(iron);
+    }
+    public void setCal(int cal){
+        this.cal = cal;
+    }
+    public void setFatCal(int fatCal){
+        this.fatCal = fatCal;
+    }
+    public void setTotFat(int totFat){
+        this.totFat = totFat;
+    }
+    public void setSatFat(int satFat){
+        this.satFat = satFat;
+    }
+    public void setTransFat(int transFat){
+        this.transFat = transFat;
+    }
+    public void setChol(int chol){
+        this.chol = chol;
+    }
+     public void setSodium(int sodium){
+        this.sodium = sodium;
+    }
+     public void setCarb(int carb){
+        this.carb = carb;
+    }
+     public void setFiber(int fiber){
+        this.fiber = fiber;
+    }
+    public void setSugars(int sugars){
+        this.sugars = sugars;
+    }
+    public void setProtein(int protein){
+        this.protein = protein;
+    }
+    public void setVitA(int vitA){
+        this.vitA = vitA;
+    }
+    public void setVitC(int vitC){
+        this.vitC = vitC;
+    }
+    public void setCalcium(int calcium){
+        this.calcium = calcium;
+    }
+    public void setIron(int iron){
+        this.iron = iron;
+    }
+    
     // String Representation:
     @Override
     public String toString() {
