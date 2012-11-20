6c8a06d44a98f631f1fefd45626a59cea51bf646
Alan Morin <root.user0@gmail.com>
2012-11-19 22:02:19 -0500

    updated HoagieServlet

--- Alan/NotWawa/src/java/hoagie/Ingredients.java
+++ Alan/NotWawa/src/java/hoagie/Ingredients.java
@@ -3,55 +3,33 @@
  * and open the template in the editor.
  */
 package hoagie;
-
+ 
 import java.io.Serializable;
+import java.sql.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-/**
- *
- * @author Alan
- */
+ 
 @Entity
 public class Ingredients implements Serializable {
     private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+ 
+    // Persistent Fields:
+    @Id @GeneratedValue
+    Long id;
+    private String name;
 
-    public Long getId() {
-        return id;
+    // Constructors:
+    public Ingredients() {
     }
-
-    public void setId(Long id) {
-        this.id = id;
+ 
+    public Ingredients(String name) {
+        this.name = name;
     }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Ingredients)) {
-            return false;
-        }
-        Ingredients other = (Ingredients) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
+ 
+    // String Representation:
     @Override
     public String toString() {
-        return "hoagie.Ingredients[ id=" + id + " ]";
+        return name + "<br/>";
     }
-    
 }
--- Alan/NotWawa/src/java/hoagie/Sandwich.java
+++ Alan/NotWawa/src/java/hoagie/Sandwich.java
@@ -3,55 +3,39 @@
  * and open the template in the editor.
  */
 package hoagie;
-
+ 
 import java.io.Serializable;
+import java.sql.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-/**
- *
- * @author Alan
- */
+ 
 @Entity
 public class Sandwich implements Serializable {
     private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    public Long getId() {
-        return id;
+ 
+    // Persistent Fields:
+    @Id @GeneratedValue
+    Long id;
+    private int hoagie_id;
+    private int ingred_id;
+    private int count;
+    private Date signingDate;
+ 
+    // Constructors:
+    public Sandwich() {
     }
-
-    public void setId(Long id) {
-        this.id = id;
+ 
+    public Sandwich(int hoagie_id, int ingred_id, int count) {
+        this.hoagie_id = hoagie_id;     //id of the hoagie 
+        this.ingred_id = ingred_id;     //id of the ingredient
+        this.count = count;             //how many slices
+        this.signingDate = new Date(System.currentTimeMillis());
     }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Sandwich)) {
-            return false;
-        }
-        Sandwich other = (Sandwich) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
+ 
+    // String Representation:
     @Override
     public String toString() {
-        return "hoagie.Sandwich[ id=" + id + " ]";
+        return "<br/>";
     }
-    
 }
