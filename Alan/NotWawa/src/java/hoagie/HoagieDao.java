d54f469956f652595ecbe9aa8a3ab0bb43a2303a
Alan Morin <root.user0@gmail.com>
2012-11-28 06:53:38 -0500

    more changes2

--- Alan/NotWawa/src/java/hoagie/HoagieDao.java
+++ Alan/NotWawa/src/java/hoagie/HoagieDao.java
@@ -1,11 +1,13 @@
 package hoagie;
  
 import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 
@@ -14,18 +16,53 @@ public class HoagieDao {
     // Injected database connection:
     @PersistenceContext private EntityManager em;
 
- 
-    // Stores a new guest: 
-    public void persist(Hoagie hoagie) {
-        //em.getTransaction().begin();
-        em.persist(hoagie);
-        //em.getTransaction().commit();
+    public HoagieDao(){
     }
- 
-    // Retrieves all the guests:
+    
+    // Stores a new hoagie: 
+    public void persist(Hoagie hoagie) {
+        em.persist(hoagie);
+    }
+
+    // Store a new ingredient
+    public void persistIngredients(HoagieIngredients ingred){
+        em.persist(ingred);
+        
+    }
+    
+    // Store a new user / password
+    public void persistUserPass(Password pass){
+        em.persist(pass);    
+    }
+    
+    // Retrieves all the hoagies:
     public List<Hoagie> getAllHoagies() {
         TypedQuery<Hoagie> query = em.createQuery(
-            "SELECT h FROM Hoagie h ORDER BY h.id", Hoagie.class);
+            "SELECT g FROM Hoagie g ORDER BY g.id", Hoagie.class);
         return query.getResultList();
     }
+    
+    // Retrieves all the ingredients:
+    public List<HoagieIngredients> getAllIngredients() {
+        TypedQuery<HoagieIngredients> query = em.createQuery(
+            "SELECT i FROM HoagieIngredients i ORDER BY i.id", HoagieIngredients.class);
+        return query.getResultList();
+    }
+    
+    // Retrieves all the ingredients:
+    public StringBuilder getUserPass(String u, StringBuilder p) {
+        Query query = em.createQuery(
+            "SELECT p.password FROM Password p WHERE p.userName = :u", String.class);
+        StringBuilder c = (StringBuilder)query.getSingleResult();
+        return c;
+    }
+    
+    // Retrieves all the ingredients:
+    public boolean verifyUserExists(String u) {
+        Query query = em.createQuery(
+            "SELECT p.userName FROM Password AS p WHERE p.userName == :u", Password.class);
+        List<Password> elementList = query.getResultList();
+        return elementList.isEmpty();
+    }
+    
 }
