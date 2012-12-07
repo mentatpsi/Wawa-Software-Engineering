55024bf5b91479e190e31f5c37c385498b6c01dc
Shay Maor <shay.h.maor@gmail.com>
2012-12-06 23:59:27 -0500

    [HoagieDao] Added Methods

--- Alan/NotWawa/src/java/hoagie/HoagieDao.java
+++ Alan/NotWawa/src/java/hoagie/HoagieDao.java
@@ -9,6 +9,7 @@ import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import java.util.ArrayList;
 
 
 @Stateless
@@ -45,6 +46,61 @@ public class HoagieDao {
         TypedQuery<Hoagie> query = em.createQuery(
             "SELECT g FROM Hoagie g ORDER BY g.id", Hoagie.class);
         return query.getResultList();
+    }
+    
+    public List<HoagieMap> getMapForHoagie(int hoag) {
+        TypedQuery<HoagieMap> query = em.createQuery(
+            "SELECT hm FROM HoagieMap hm where hm.hoagie_id = :hoag", HoagieMap.class);
+        
+        return query.getResultList(); 
+    }
+    
+    public HoagieIngredients getTotal(int hoag) {
+        List<HoagieIngredients> hingrs = this.getIngrsForHoagie(hoag);
+        HoagieIngredients hoagieIngr = new HoagieIngredients();
+        int calories = 0;
+        int totalFat = 0;
+        int saturatedFat = 0;
+        int carbs = 0;
+        int fiber = 0;
+        int protein = 0;
+        
+        
+        for (HoagieIngredients hIngr : hingrs) {
+            calories+= hIngr.getCalories();
+            totalFat+= hIngr.getTotalFat();
+            saturatedFat+= hIngr.getSatFat();
+            carbs+= hIngr.getCarbs();
+            fiber+= hIngr.getFiber();
+            protein+= hIngr.getProtein();
+            
+            
+        }
+        
+        hoagieIngr.setCalories(calories);
+        hoagieIngr.setCarbs(carbs);
+        hoagieIngr.setFiber(fiber);
+        hoagieIngr.setSatFat(saturatedFat);
+        hoagieIngr.setProtein(protein);
+        
+        hoagieIngr.setId(Long.getLong(String.valueOf(hoag))); //TODO: Might be goot to give a value
+        
+        return hoagieIngr;
+    }
+
+    
+    public List<HoagieIngredients> getIngrsForHoagie(int hoag) {
+        List<HoagieMap> hMaps = this.getMapForHoagie(hoag);
+        ArrayList<HoagieIngredients> hIngrs = new ArrayList();
+        
+        for (HoagieMap hMap : hMaps) {
+            int curIngr = hMap.getIngredient_id();
+            TypedQuery<HoagieIngredients> ingrQuery = em.createQuery(
+            "SELECT hi FROM HoagieIngredients hi where hi.id = :curIngr", HoagieIngredients.class);
+            
+            hIngrs.add(ingrQuery.getSingleResult());
+        }
+        return hIngrs;
     }
     
     // Retrieves all the ingredients:
